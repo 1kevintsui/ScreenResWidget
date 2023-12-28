9fcc7811e1b42ed16d75ceba935e3ae625a026e5
1kevintsui <70086242+1kevintsui@users.noreply.github.com>
2023-12-27 19:35:55 -0800

    updates title

--- DeskWidget.py
+++ DeskWidget.py
@@ -38,10 +38,12 @@ class Window(Tk.Tk):
     def resolution_one(self):
         sp.call(f"./qres.exe /x {self.res1x} /y {self.res1y}")
         self.resolution_toggle = False
+        self.title(f"{self.res1x}x{self.res1y}")
         
     def resolution_two(self):
         sp.call(f"./qres.exe /x {self.res2x} /y {self.res2y} ")
         self.resolution_toggle = True
+        self.title(f"{self.res2x}x{self.res2y}")
         
     def toggle(self):
         if self.resolution_toggle:
