64108af4e36b9358013fb85d17db2baa16a8cfc9
1kevintsui <70086242+1kevintsui@users.noreply.github.com>
2024-01-01 11:25:05 -0800

    Added another resolution to widget
    added a new resolution

--- DeskWidget.py
+++ DeskWidget.py
@@ -2,6 +2,7 @@ import tkinter as Tk
 import subprocess as sp
 import os as os
 import configparser as cp
+from functools import partial
 from ahk import AHK
 
 class Window(Tk.Tk):
@@ -14,26 +15,63 @@ class Window(Tk.Tk):
         self.res1y = config['Resolution One']['height']
         self.res2x = config['Resolution Two']['width']
         self.res2y = config['Resolution Two']['height']
+        self.res3x = config['Resolution Three']['width']
+        self.res3y = config['Resolution Three']['height']
         
         key1 = config['HotKey']['key1']
         key2 = config['HotKey']['key2']
         self.key = self.__assignkey(key1) + key2
-        self.ahk.add_hotkey(self.key, callback=self.toggle)
+        self.ahk.add_hotkey(self.key, callback=partial(self.resolution_changer,
+                                                            flip=True))
         self.ahk.start_hotkeys()  # start the hotkey process thread
         self.geometry("250x50")
         self.title(f"{config['Title']['Title']}")
-        self.resizable(0,0)
+        self.resizable(1,1)
         self.columnconfigure(index=0, weight=1)
         self.columnconfigure(index=1, weight=1)
-        self.res1 = Tk.Button(self,command=self.resolution_one)
+        self.columnconfigure(index=2, weight=1)
+        
+        self.res1 = Tk.Button(self,command=partial(self.resolution_changer,
+                                                   resx=self.res1x, rexy=self.res1y))
         self.res1.configure(text=f"{self.res1x}x{self.res1y}")
-        self.res2 = Tk.Button(self,command=self.resolution_two)
+        
+        self.res2 = Tk.Button(self,command=partial(self.resolution_changer,
+                                                   resx=self.res2x, rexy=self.res2y))
         self.res2.configure(text=f"{self.res2x}x{self.res2y}")
+        
+        self.res3 = Tk.Button(self,command=partial(self.resolution_changer,
+                                                  resx=self.res3x, rexy=self.res3y))
+        self.res3.configure(text=f"{self.res3x}x{self.res3y}")
+        
         self.res1.configure(height=3)
         self.res1.grid(row=0, column=0, sticky='nsew')
         self.res2.grid(row=0, column=1, sticky='nsew')
-        self.resolution_toggle = True
+        self.res3.grid(row=0, column=2, sticky='nsew')
+        self.resolution_toggle = 0
         self.update()
+        
+    def test(self):
+        print("hotkey")
+        
+    def resolution_changer(self, flip=False, resx=0, resy=0):
+        if flip:
+            print(self.resolution_toggle)
+            if self.resolution_toggle % 3 == 0:
+                sp.call(f"./qres.exe /x {self.res1x} /y {self.res1y}")
+                self.title(f"{self.res1x}x{self.res1y}")
+            elif self.resolution_toggle % 3 == 1:
+                sp.call(f"./qres.exe /x {self.res2x} /y {self.res2y}")
+                self.title(f"{self.res2x}x{self.res2y}")
+            else:
+                sp.call(f"./qres.exe /x {self.res3x} /y {self.res3y}")
+                self.title(f"{self.res3x}x{self.res3y}")
+            self.resolution_toggle += 1
+            if self.resolution_toggle > 5:
+                self.resolution_toggle = 0
+        else:
+            sp.call(f"./qres.exe /x {resx} /y {resy}")
+            self.title(f"{resx}x{resy}")
+            
 
     def resolution_one(self):
         sp.call(f"./qres.exe /x {self.res1x} /y {self.res1y}")
@@ -45,13 +83,25 @@ class Window(Tk.Tk):
         self.resolution_toggle = True
         self.title(f"{self.res2x}x{self.res2y}")
         
+    def resolution_three(self):
+        sp.call(f"./qres.exe /x {self.res3x} /y {self.res3y} ")
+        self.resolution_toggle = True
+        self.title(f"{self.res3x}x{self.res3y}")
+        
     def toggle(self):
-        if self.resolution_toggle:
+        print(self.resolution_toggle)
+        if self.resolution_toggle % 3 == 0:
             self.resolution_toggle = False
             self.resolution_one()
-        else:
+        elif self.resolution_toggle % 3 == 1:
             self.resolution_toggle = True
             self.resolution_two()
+        else:
+            self.resolution_toggle = True
+            self.resolution_three()
+        self.resolution_toggle += 1
+        if self.resolution_toggle > 5:
+            self.resolution_toggle = 0
     
     def __del__(self):
         pass
